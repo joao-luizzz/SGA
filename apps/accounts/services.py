32678bc06903a22953985410fb61311246b75164
Andrey Kerges <andreykergesprofissional@gmail.com>
2026-08-21 21:46:28 -0300

    feat(accounts): implementa servico de inativacao de usuario

--- apps/accounts/services.py
+++ apps/accounts/services.py
@@ -37,3 +37,9 @@ def change_user_password(user: CustomUser, new_password: str) -> CustomUser:
     user.must_change_password = False
     user.save(update_fields=['password', 'must_change_password'])
     return user
+
+def toggle_user_active_status(user: CustomUser) -> CustomUser:
+    """Alterna o status de ativação do usuário."""
+    user.is_active = not user.is_active
+    user.save(update_fields=['is_active'])
+    return user
